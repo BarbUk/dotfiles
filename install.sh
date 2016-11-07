34d791b3c0ecd4d8399f909fda670796b1934293
BarbUk <julien.virey@gmail.com>
2016-11-07 08:45:05 +0400

    add bash-it and z as submodule install.sh symlink the files

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -e -o pipefail -o nounset
+
+readonly dotfiles_dir="$HOME/.dotfiles"
+
+ln -sf "$dotfiles_dir/bashrc" "$HOME/.bashrc"
+ln -sf "$dotfiles_dir/tmux.conf" "$HOME/.tmux.conf"
+ln -sf "$dotfiles_dir/inputrc" "$HOME/.inputrc"
+
+ln -sf "$dotfiles_dir/bash-it" "$HOME/.bash_it"
+ln -sf "$dotfiles_dir/z/z.sh" "$HOME/.rupaz"
