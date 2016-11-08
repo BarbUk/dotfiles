0a04b64cd18312aa941736b9c6ef2a1b04bbb46f
BarbUk <julien.virey@gmail.com>
2016-11-08 10:42:32 +0400

    move submodules in modules dir

--- install.sh
+++ install.sh
@@ -8,8 +8,8 @@ ln -sf "$dotfiles_dir/bashrc" "$HOME/.bashrc"
 ln -sf "$dotfiles_dir/tmux.conf" "$HOME/.tmux.conf"
 ln -sf "$dotfiles_dir/inputrc" "$HOME/.inputrc"
 
-ln -sf "$dotfiles_dir/bash-it" "$HOME/.bash_it"
-ln -sf "$dotfiles_dir/z/z.sh" "$HOME/.rupaz"
+ln -sf "$dotfiles_dir/modules/bash-it" "$HOME/.bash_it"
+ln -sf "$dotfiles_dir/modules/z/z.sh" "$HOME/.rupaz"
 
 mkdir -p "$HOME/.tmux/plugins"
-ln -sf "$dotfiles_dir/tpm" "$HOME/.tmux/plugins/"
+ln -sf "$dotfiles_dir/modules/tpm" "$HOME/.tmux/plugins/"
