5a37eae8a0d3696a27e042b1f6ea984979b261ab
BarbUk <julien.virey@gmail.com>
2017-01-04 01:30:02 +0400

    big cleaning and new apps dotfiles

--- install.sh
+++ install.sh
@@ -4,14 +4,24 @@ set -e -o pipefail -o nounset
 
 readonly dotfiles_dir="$HOME/.dotfiles"
 
-ln -sf "$dotfiles_dir/bashrc" "$HOME/.bashrc"
-ln -sf "$dotfiles_dir/tmux.conf" "$HOME/.tmux.conf"
-ln -sf "$dotfiles_dir/inputrc" "$HOME/.inputrc"
+mkdir -p "$HOME/{.vim,.tmux/plugins,.mpv,.mpd,.ncmpcpp}"
+
+ln -sf "$dotfiles_dir/system/xinitrc" "$HOME/.xinitrc"
+
+ln -sf "$dotfiles_dir/shell/bashrc" "$HOME/.bashrc"
+ln -sf "$dotfiles_dir/shell/inputrc" "$HOME/.inputrc"
 
 ln -sf "$dotfiles_dir/modules/bash-it" "$HOME/.bash_it"
 ln -sf "$dotfiles_dir/modules/z/z.sh" "$HOME/.rupaz"
 
-ln -sf "$dotfiles_dir/vimrc" "$HOME/.vimrc"
+ln -sf "$dotfiles_dir/apps/tmux.conf" "$HOME/.tmux.conf"
+ln -sf "$dotfiles_dir/apps/vimrc" "$HOME/.vimrc"
+ln -sf "$dotfiles_dir/apps/vimrc.dark" "$HOME/.vim/vimrc.background"
+ln -sf "$dotfiles_dir/apps/termite.dark" "$HOME/.config/termite/config"
+ln -sf "$dotfiles_dir/apps/mpv" "$HOME/.mpv/config"
+ln -sf "$dotfiles_dir/apps/mpd.conf" "$HOME/.mpd/mpd.conf"
+ln -sf "$dotfiles_dir/apps/ncmpcpp" "$HOME/.ncmpcpp/config"
+ln -sf "$dotfiles_dir/apps/compton.conf" "$HOME/.compton.conf"
+ln -sf "$dotfiles_dir/apps/gitconfig" "$HOME/.gitconfig"
 
-mkdir -p "$HOME/.tmux/plugins"
 ln -sf "$dotfiles_dir/modules/tpm" "$HOME/.tmux/plugins/"
