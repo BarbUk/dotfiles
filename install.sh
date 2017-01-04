b2c52ad61721dce5507b2c143f8f6dc3decdfd95
BarbUk <julien.virey@gmail.com>
2017-01-04 11:19:22 +0400

    install.sh OSX compat

--- install.sh
+++ install.sh
@@ -4,24 +4,29 @@ set -e -o pipefail -o nounset
 
 readonly dotfiles_dir="$HOME/.dotfiles"
 
-mkdir -p "$HOME/{.vim,.tmux/plugins,.mpv,.mpd,.ncmpcpp}"
+if [ "$(uname)" = "Darwin" ]; then
+    LN='ln -sf'
+    mkdir -p "$HOME"/{.vim,.tmux/plugins}
+else
+    LN='ln -srf'
+    mkdir -p "$HOME"/{.vim,.tmux/plugins,.mpv,.mpd,.ncmpcpp}
+    ${LN} "$dotfiles_dir/system/xinitrc" "$HOME/.xinitrc"
+    ${LN} "$dotfiles_dir/apps/mpv" "$HOME/.mpv/config"
+    ${LN} "$dotfiles_dir/apps/mpd.conf" "$HOME/.mpd/mpd.conf"
+    ${LN} "$dotfiles_dir/apps/ncmpcpp" "$HOME/.ncmpcpp/config"
+    ${LN} "$dotfiles_dir/apps/compton.conf" "$HOME/.compton.conf"
+    ${LN} "$dotfiles_dir/apps/termite.dark" "$HOME/.config/termite/config"
+fi
 
-ln -srf "$dotfiles_dir/system/xinitrc" "$HOME/.xinitrc"
+${LN} "$dotfiles_dir/shell/bashrc" "$HOME/.bashrc"
+${LN} "$dotfiles_dir/shell/inputrc" "$HOME/.inputrc"
 
-ln -srf "$dotfiles_dir/shell/bashrc" "$HOME/.bashrc"
-ln -srf "$dotfiles_dir/shell/inputrc" "$HOME/.inputrc"
+${LN}n "$dotfiles_dir/modules/bash-it" "$HOME/.bash_it"
+${LN} "$dotfiles_dir/modules/z/z.sh" "$HOME/.rupaz"
 
-ln -srfn "$dotfiles_dir/modules/bash-it" "$HOME/.bash_it"
-ln -srf "$dotfiles_dir/modules/z/z.sh" "$HOME/.rupaz"
+${LN} "$dotfiles_dir/apps/tmux.conf" "$HOME/.tmux.conf"
+${LN} "$dotfiles_dir/apps/vimrc" "$HOME/.vimrc"
+${LN} "$dotfiles_dir/apps/vimrc.dark" "$HOME/.vim/vimrc.background"
+${LN} "$dotfiles_dir/apps/gitconfig" "$HOME/.gitconfig"
 
-ln -srf "$dotfiles_dir/apps/tmux.conf" "$HOME/.tmux.conf"
-ln -srf "$dotfiles_dir/apps/vimrc" "$HOME/.vimrc"
-ln -srf "$dotfiles_dir/apps/vimrc.dark" "$HOME/.vim/vimrc.background"
-ln -srf "$dotfiles_dir/apps/termite.dark" "$HOME/.config/termite/config"
-ln -srf "$dotfiles_dir/apps/mpv" "$HOME/.mpv/config"
-ln -srf "$dotfiles_dir/apps/mpd.conf" "$HOME/.mpd/mpd.conf"
-ln -srf "$dotfiles_dir/apps/ncmpcpp" "$HOME/.ncmpcpp/config"
-ln -srf "$dotfiles_dir/apps/compton.conf" "$HOME/.compton.conf"
-ln -srf "$dotfiles_dir/apps/gitconfig" "$HOME/.gitconfig"
-
-ln -srf "$dotfiles_dir/modules/tpm" "$HOME/.tmux/plugins/"
+${LN} "$dotfiles_dir/modules/tpm" "$HOME/.tmux/plugins/"
