d080d07fc8829e17b2e51c59a7473d1c7b648e8c
BarbUk <julien.virey@gmail.com>
2017-04-28 18:38:33 +0400

    keep bash_it symlink in my home

--- install.sh
+++ install.sh
@@ -26,7 +26,7 @@ ${LN} "$dotfiles_dir/shell/inputrc" "$HOME/.inputrc"
 modules_dir="$HOME/.modules"
 mkdir -p "$modules_dir"
 
-${LN}n "$dotfiles_dir/modules/bash-it" "$modules_dir/bash_it"
+${LN}n "$dotfiles_dir/modules/bash-it" "$HOME/.bash_it"
 ${LN} "$dotfiles_dir/modules/z/z.sh" "$modules_dir/z"
 ${LN} "$dotfiles_dir/modules/goto/goto.sh" "$modules_dir/goto"
 ${LN} "$dotfiles_dir/modules/fz/fz.sh" "$modules_dir/fz"
