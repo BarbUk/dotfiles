c829f7de059b598401952fa6ce672a316427130b
BarbUk <julien.virey@gmail.com>
2025-11-11 18:35:16 +0400

    Battery and formatting

--- apps/awesome/config/widgets/init.lua
+++ apps/awesome/config/widgets/init.lua
@@ -1,234 +1,255 @@
-local awful      = require("awful")
-local wibox      = require("wibox")
-local lain       = require("lain")
-local helpers    = require("lain.helpers")
-local beautiful  = require("beautiful")
+local awful = require("awful")
+local wibox = require("wibox")
+local lain = require("lain")
+local helpers = require("lain.helpers")
+local beautiful = require("beautiful")
 local xresources = require("beautiful.xresources")
-local dpi        = xresources.apply_dpi
+local dpi = xresources.apply_dpi
 
-local apps       = require('config.apps')
-local bat        = require('config.widgets.battery')
-local volume     = require('config.widgets.volume')
-local mpris_info = require('config.widgets.mpris')
+local apps = require("config.apps")
+local bat = require("config.widgets.battery")
+local volume = require("config.widgets.volume")
+local mpris_info = require("config.widgets.mpris")
 
 -- {{{ Wibox
 local markup = lain.util.markup
-local white  = beautiful.fg_normal
-local gray   = beautiful.fg_focus
+local white = beautiful.fg_normal
+local gray = beautiful.fg_focus
 
 -- Separators
 local separator = {
-  left = wibox.widget.textbox(markup(white, markup.bold('  '))),
-  center = wibox.widget.textbox(markup(white, ' ')),
-  right = wibox.widget.textbox(markup(white, markup.bold('  '))),
+   left = wibox.widget.textbox(markup(white, markup.bold("  "))),
+   center = wibox.widget.textbox(markup(white, " ")),
+   right = wibox.widget.textbox(markup(white, markup.bold("  "))),
 }
 
 -- flags and timezone
 local flag = {}
 local clock = {}
 for k, v in pairs(beautiful.flag) do
-  flag[k] = wibox.widget.imagebox(beautiful.flag[k], true)
-  flag[k]:buttons( awful.util.table.join (
-    awful.button({ }, 1, function()
+   flag[k] = wibox.widget.imagebox(beautiful.flag[k], true)
+   flag[k]:buttons(awful.util.table.join(awful.button({}, 1, function()
       awful.spawn(apps.cmd.timezone[k], false)
-    end)
-  ))
-  clock[k] = wibox.widget.textclock(markup(white, "%H:%M"), 30, apps.timezone[k])
+   end)))
+   clock[k] = wibox.widget.textclock(markup(white, "%H:%M"), 30, apps.timezone[k])
 end
 
 -- Calendar
 local calendar = lain.widget.cal({
-  attach_to = { clock.france, clock.canada, clock.mauritius, clock.cambodia },
-  three     = true,
-  icons     = helpers.icons_dir .. "cal/" .. beautiful.iconcolor .. "/",
-  followtag = true,
-  notification_preset = {
-    font = beautiful.notification_font,
-    fg   = beautiful.notification_fg,
-    bg   = beautiful.notification_bg
-}})
+   attach_to = { clock.france, clock.canada, clock.mauritius, clock.cambodia },
+   three = true,
+   icons = helpers.icons_dir .. "cal/" .. beautiful.iconcolor .. "/",
+   followtag = true,
+   notification_preset = {
+      font = beautiful.notification_font,
+      fg = beautiful.notification_fg,
+      bg = beautiful.notification_bg,
+   },
+})
 
 -- mpris
 local mpris = mpris_info({
-  cmd = "mprisctl",
-  cover_size = 80,
-  notification_preset = {
-    font  = "PragmataPro Liga " .. dpi(12),
-    fg    = beautiful.notification_fg,
-    bg    = beautiful.notification_bg,
-  },
-  settings = function()
-    if mpris_now.status ~= "no player detected" then
-      state = "  "
-      if mpris_now.status == "Playing" then
-        state = markup(beautiful.nord7, "  ")
-      elseif mpris_now.status == "Paused" then
-        state = markup(beautiful.nord7, "  ")
+   cmd = "mprisctl",
+   cover_size = 80,
+   notification_preset = {
+      font = "PragmataPro Nerd Font " .. dpi(12),
+      fg = beautiful.notification_fg,
+      bg = beautiful.notification_bg,
+   },
+   settings = function()
+      if mpris_now.status ~= "no player detected" then
+         state = "  "
+         if mpris_now.status == "Playing" then
+            state = markup(beautiful.nord7, "  ")
+         elseif mpris_now.status == "Paused" then
+            state = markup(beautiful.nord7, "  ")
+         end
+         widget:set_markup(
+            state .. markup(white, mpris_now.artist) .. " - " .. markup(white, mpris_now.title) .. " "
+         )
+      else
+         widget:set_markup("")
       end
-      widget:set_markup(state .. markup(white, mpris_now.artist) .. " - " .. markup(white, mpris_now.title) .. " ")
-    else
-      widget:set_markup("")
-    end
-  end
+   end,
 })
 
-mpris.widget:buttons( awful.util.table.join (
-  awful.button({ }, 1, function()
-    awful.util.spawn(apps.cmd.player.toggle, false, mpris.update())
-  end),
-  awful.button({ }, 4, function()
-    awful.util.spawn(apps.cmd.player.next, false, mpris.update())
-  end),
-  awful.button({ }, 5, function()
-    awful.util.spawn(apps.cmd.player.prev, false, mpris.update())
-  end)
+mpris.widget:buttons(awful.util.table.join(
+   awful.button({}, 1, function()
+      awful.util.spawn(apps.cmd.player.toggle, false, mpris.update())
+   end),
+   awful.button({}, 4, function()
+      awful.util.spawn(apps.cmd.player.next, false, mpris.update())
+   end),
+   awful.button({}, 5, function()
+      awful.util.spawn(apps.cmd.player.prev, false, mpris.update())
+   end)
 ))
 
 -- /home fs
 local fsroot = lain.widget.fs({
-  notification_preset = { fg = white, bg = beautiful.bg_normal },
-  settings  = function()
-    hdd = ""
-    p   = ""
+   notification_preset = { fg = white, bg = beautiful.bg_normal },
+   settings = function()
+      hdd = ""
+      p = ""
 
-    if fs_now["/"].percentage >= 85 then
-      hdd = markup(white, markup.bold('  ')) .. markup(beautiful.nord7,"  ")
-      p   = fs_now["/"].percentage .. "%"
-    end
+      if fs_now["/"].percentage >= 85 then
+         hdd = markup(white, markup.bold("  ")) .. markup(beautiful.nord7, "  ")
+         p = fs_now["/"].percentage .. "%"
+      end
 
-    widget:set_markup(hdd .. markup(white, p))
-  end
+      widget:set_markup(hdd .. markup(white, p))
+   end,
 })
 
 -- Weather
 local weather = lain.widget.weather({
-  lat =  -20.4172,
-  lon = 57.5413,
-  APPID = "869faf1aac21d680efcfd1a6ce1ece93",
-  cnt = 5,
-  units = "metric",
-  lang = "fr",
-  settings = function()
-    descr = weather_now["weather"][1]["description"]
-    units = math.floor(weather_now["main"]["temp"])
-    widget:set_markup(" " .. units .. "°C")
-  end,
-  notification_text_fun = function (wn)
-    local day = os.date("%a %d", wn["dt"])
-    local temp = math.floor(wn["main"]["temp"])
-    local humi = math.floor(wn["main"]["humidity"])
-    local wind = math.floor(wn["wind"]["speed"])
-    local desc = wn["weather"][1]["description"]
-    local icon = "w" .. wn["weather"][1]["icon"]
-    return string.format("<b>%s  %s</b>:%d°C%d%%%d %s", apps.weather[icon], day, temp, humi, wind, desc)
-  end
+   lat = -20.4172,
+   lon = 57.5413,
+   APPID = "869faf1aac21d680efcfd1a6ce1ece93",
+   cnt = 5,
+   units = "metric",
+   lang = "fr",
+   settings = function()
+      descr = weather_now["weather"][1]["description"]
+      units = math.floor(weather_now["main"]["temp"])
+      widget:set_markup(" " .. units .. "°C")
+   end,
+   notification_text_fun = function(wn)
+      local day = os.date("%a %d", wn["dt"])
+      local temp = math.floor(wn["main"]["temp"])
+      local humi = math.floor(wn["main"]["humidity"])
+      local wind = math.floor(wn["wind"]["speed"])
+      local desc = wn["weather"][1]["description"]
+      local icon = "w" .. wn["weather"][1]["icon"]
+      return string.format("<b>%s  %s</b>:%d°C%d%%%d %s", apps.weather[icon], day, temp, humi, wind, desc)
+   end,
 })
 
 -- Net
 local net = lain.widget.net({
-  settings = function()
-  if iface ~= "network off" and
-    string.match(weather.widget.text, "N/A")
-  then
-    weather.update()
-  end
+   settings = function()
+      if iface ~= "network off" and string.match(weather.widget.text, "N/A") then
+         weather.update()
+      end
 
-  widget:set_markup(markup(beautiful.nord14, markup.bold("↓ ")) .. net_now.received .. " " .. markup(beautiful.nord11, markup.bold("↑ ")) .. net_now.sent)
-  end
+      widget:set_markup(
+         markup(beautiful.nord14, markup.bold("↓ "))
+            .. net_now.received
+            .. " "
+            .. markup(beautiful.nord11, markup.bold("↑ "))
+            .. net_now.sent
+      )
+   end,
 })
 
 -- CPU
 local cpuicon = wibox.widget.textbox(markup(beautiful.nord14, " "))
 local cpu = lain.widget.sysload({
-  settings = function()
-    widget:set_markup(load_1 .. " " .. load_5)
-  end
+   settings = function()
+      widget:set_markup(load_1 .. " " .. load_5)
+   end,
 })
 
 awful.screen.connect_for_each_screen(function(s)
-  -- Create the wibox
-  s.mywibox = awful.wibar({ position = "top", screen = s, height = beautiful.wibar_height })
-  s.mypromptbox = awful.widget.prompt()
+   -- Create the wibox
+   s.mywibox = awful.wibar({ position = "top", screen = s, height = beautiful.wibar_height })
+   s.mypromptbox = awful.widget.prompt()
 
-  local left = { -- Left widgets
-    layout = wibox.layout.fixed.horizontal,
-    s.mytaglist,
-    separator.right,
-    s.mytxtlayoutbox,
-    separator.right,
-    s.mypromptbox,
-  }
+   local left = { -- Left widgets
+      layout = wibox.layout.fixed.horizontal,
+      s.mytaglist,
+      separator.right,
+      s.mytxtlayoutbox,
+      separator.right,
+      s.mypromptbox,
+   }
 
-  local right = { -- Right widgets
-    layout = wibox.layout.fixed.horizontal,
-    mpris.widget,
-    separator.center,
-    wibox.widget.systray(),
-    separator.left,
-    net.widget,
-    separator.left,
-    cpuicon,
-    cpu.widget,
-    fsroot.widget,
-    separator.left,
-    bat.icon,
-    bat.widget,
-    separator.left,
-    volume.icon,
-    volume.widget,
-    separator.left,
-    weather.icon,
-    weather.widget,
-    separator.left,
-    flag.canada,separator.center,clock.canada,
-    separator.left,
-    flag.france,separator.center,clock.france,
-    separator.left,
-    flag.mauritius,separator.center,clock.mauritius,
-    separator.center,
-    flag.cambodia,separator.center,clock.cambodia,
-    separator.center,
-  }
+   local right = { -- Right widgets
+      layout = wibox.layout.fixed.horizontal,
+      mpris.widget,
+      separator.center,
+      wibox.widget.systray(),
+      separator.left,
+      net.widget,
+      separator.left,
+      cpuicon,
+      cpu.widget,
+      fsroot.widget,
+      separator.left,
+      bat.icon,
+      bat.widget,
+      separator.left,
+      volume.icon,
+      volume.widget,
+      separator.left,
+      weather.icon,
+      weather.widget,
+      separator.left,
+      flag.canada,
+      separator.center,
+      clock.canada,
+      separator.left,
+      flag.france,
+      separator.center,
+      clock.france,
+      separator.left,
+      flag.mauritius,
+      separator.center,
+      clock.mauritius,
+      separator.center,
+      flag.cambodia,
+      separator.center,
+      clock.cambodia,
+      separator.center,
+   }
 
-  local general_info = {
-    layout = wibox.layout.fixed.horizontal,
-    mpris.widget,
-    separator.center,
-    weather.icon,
-    weather.widget,
-    separator.left,
-    flag.canada,separator.center,clock.canada,
-    separator.left,
-    flag.france,separator.center,clock.france,
-    separator.left,
-    flag.mauritius,separator.center,clock.mauritius,
-    separator.left,
-    flag.cambodia,separator.center,clock.cambodia,
-    separator.center,
-  }
+   local general_info = {
+      layout = wibox.layout.fixed.horizontal,
+      mpris.widget,
+      separator.center,
+      weather.icon,
+      weather.widget,
+      separator.left,
+      flag.canada,
+      separator.center,
+      clock.canada,
+      separator.left,
+      flag.france,
+      separator.center,
+      clock.france,
+      separator.left,
+      flag.mauritius,
+      separator.center,
+      clock.mauritius,
+      separator.left,
+      flag.cambodia,
+      separator.center,
+      clock.cambodia,
+      separator.center,
+   }
 
-  if s.index == 1 then
-    -- Add widgets to the wibox
-    s.mywibox:setup {
-      layout = wibox.layout.align.horizontal,
-      left,
-      s.mytasklist, -- Middle widget
-      right
-  }
-  else
-    -- Add widgets to the wibox
-    s.mywibox:setup {
-      layout = wibox.layout.align.horizontal,
-      left,
-      s.mytasklist, -- Middle widget,
-      general_info
-    }
-  end
+   if s.index == 1 then
+      -- Add widgets to the wibox
+      s.mywibox:setup({
+         layout = wibox.layout.align.horizontal,
+         left,
+         s.mytasklist, -- Middle widget
+         right,
+      })
+   else
+      -- Add widgets to the wibox
+      s.mywibox:setup({
+         layout = wibox.layout.align.horizontal,
+         left,
+         s.mytasklist, -- Middle widget,
+         general_info,
+      })
+   end
 end)
 
 return {
-  mpris    = mpris,
-  volume   = volume.helper,
-  calendar = calendar,
-  fsroot   = fsroot,
+   mpris = mpris,
+   volume = volume.helper,
+   calendar = calendar,
+   fsroot = fsroot,
 }
--- apps/awesome/themes/barbuk/base.lua
+++ apps/awesome/themes/barbuk/base.lua
@@ -3,20 +3,20 @@
      BarbUk Awesome WM config dark
 
 --]]
-local helpers    = require("config.helpers")
-local home       = os.getenv("HOME")
+local helpers = require("config.helpers")
+local home = os.getenv("HOME")
 local light_file = home .. "/.config/themelight"
-local color      = "dark"
+local color = "dark"
 
 if helpers.file_exists(light_file) then
-  color = "light"
+   color = "light"
 end
 
-local theme               = require("themes/barbuk/" .. color)
-local gears               = require("gears")
-local awful               = require("awful")
-local xresources          = require("beautiful.xresources")
-local dpi                 = xresources.apply_dpi
+local theme = require("themes/barbuk/" .. color)
+local gears = require("gears")
+local awful = require("awful")
+local xresources = require("beautiful.xresources")
+local dpi = xresources.apply_dpi
 
 -- Do not scale with dpi
 theme.wibar_height = dpi(15)
@@ -24,145 +24,145 @@ theme.notification_max_width = dpi(720)
 theme.notification_icon_size = dpi(64)
 
 awful.screen.connect_for_each_screen(function(s)
-  if s.outputs["eDP"] then
-    xresources.set_dpi(192, s)
-    dpi = xresources.apply_dpi
-    theme.wibar_height = dpi(30)
-    theme.notification_max_width = dpi(1440)
-    theme.notification_icon_size = dpi(128)
-  end
+   if s.outputs["eDP"] then
+      xresources.set_dpi(192, s)
+      dpi = xresources.apply_dpi
+      theme.wibar_height = dpi(30)
+      theme.notification_max_width = dpi(1440)
+      theme.notification_icon_size = dpi(128)
+   end
 end)
 
 awful.screen.set_auto_dpi_enabled(true)
 
-theme.dir                 = home .. "/.config/awesome/themes/barbuk"
-theme.wallpaper           = theme.dir .. "/archlinux.png"
+theme.dir = home .. "/.config/awesome/themes/barbuk"
+theme.wallpaper = theme.dir .. "/archlinux.png"
 
-theme.font                = "PragmataPro Liga " .. dpi(8)
+theme.font = "PragmataPro Nerd Font " .. dpi(8)
 
-theme.fg_normal           = theme.foreground
-theme.fg_focus            = theme.focus
-theme.bg_normal           = theme.background
-theme.bg_focus            = theme.background
-theme.fg_urgent           = theme.background
-theme.bg_urgent           = theme.nord11
-theme.border_width        = dpi(1)
-theme.border_normal       = theme.nord1
-theme.border_focus        = theme.nord10
-theme.taglist_fg_focus    = theme.foreground
-theme.taglist_bg_focus    = theme.nord9
+theme.fg_normal = theme.foreground
+theme.fg_focus = theme.focus
+theme.bg_normal = theme.background
+theme.bg_focus = theme.background
+theme.fg_urgent = theme.background
+theme.bg_urgent = theme.nord11
+theme.border_width = dpi(1)
+theme.border_normal = theme.nord1
+theme.border_focus = theme.nord10
+theme.taglist_fg_focus = theme.foreground
+theme.taglist_bg_focus = theme.nord9
 theme.taglist_fg_occupied = theme.nord9
-theme.menu_height         = dpi(12)
-theme.menu_width          = dpi(140)
-theme.menu_border_width   = dpi(0)
-theme.useless_gap         = dpi(0.2)
+theme.menu_height = dpi(12)
+theme.menu_width = dpi(140)
+theme.menu_border_width = dpi(0)
+theme.useless_gap = dpi(0.2)
 
 -- Rounded corners
-theme.border_radius       = dpi(6)
-theme.screen_margin       = dpi(5)
+theme.border_radius = dpi(6)
+theme.screen_margin = dpi(5)
 
-theme.notification_position         = "top_right"
-theme.notification_border_width     = dpi(2)
-theme.notification_border_radius    = theme.border_radius
-theme.notification_border_color     = theme.nord14
-theme.notification_bg               = theme.background
-theme.notification_fg               = theme.foreground
-theme.notification_crit_bg          = theme.bg_urgent
-theme.notification_crit_fg          = theme.fg_urgent
-theme.notification_margin           = dpi(12)
-theme.notification_font             = "PragmataPro Liga " .. dpi(11)
-theme.notification_padding          = theme.screen_margin * 2
-theme.notification_spacing          = theme.screen_margin * 2
-theme.notification_shape            = helpers.rrect(theme.notification_border_radius)
+theme.notification_position = "top_right"
+theme.notification_border_width = dpi(2)
+theme.notification_border_radius = theme.border_radius
+theme.notification_border_color = theme.nord14
+theme.notification_bg = theme.background
+theme.notification_fg = theme.foreground
+theme.notification_crit_bg = theme.bg_urgent
+theme.notification_crit_fg = theme.fg_urgent
+theme.notification_margin = dpi(12)
+theme.notification_font = "PragmataPro Nerd Font " .. dpi(11)
+theme.notification_padding = theme.screen_margin * 2
+theme.notification_spacing = theme.screen_margin * 2
+theme.notification_shape = helpers.rrect(theme.notification_border_radius)
 
-theme.ocol                          = "<span color='" .. theme.nord8 .. "'>"
-theme.ccol                          = "</span>"
-theme.tasklist_ontop                = theme.ocol .. " " .. theme.ccol
-theme.tasklist_floating             = theme.ocol .. " " .. theme.ccol
+theme.ocol = "<span color='" .. theme.nord8 .. "'>"
+theme.ccol = "</span>"
+theme.tasklist_ontop = theme.ocol .. " " .. theme.ccol
+theme.tasklist_floating = theme.ocol .. " " .. theme.ccol
 theme.tasklist_maximized_horizontal = theme.ocol .. " " .. theme.ccol
-theme.tasklist_maximized_vertical   = theme.ocol .. " " .. theme.ccol
-theme.tasklist_maximized            = theme.ocol .. " " .. theme.ccol
-theme.layout_txt_tile               = ""
-theme.layout_txt_tiletop            = ""
-theme.layout_txt_fairv              = ""
-theme.layout_txt_dwindle            = ""
-theme.layout_txt_fullscreen         = ""
-theme.layout_txt_floating           = ""
-theme.tasklist_disable_icon         = false
-theme.icon_theme                    = "Papirus"
-theme.flag                          = {
-  france                            = theme.dir .. "/icons/fr.png",
-  mauritius                         = theme.dir .. "/icons/mu.png",
-  canada                            = theme.dir .. "/icons/ca.png",
-  cambodia                          = theme.dir .. "/icons/kh.png",
+theme.tasklist_maximized_vertical = theme.ocol .. " " .. theme.ccol
+theme.tasklist_maximized = theme.ocol .. " " .. theme.ccol
+theme.layout_txt_tile = ""
+theme.layout_txt_tiletop = ""
+theme.layout_txt_fairv = ""
+theme.layout_txt_dwindle = ""
+theme.layout_txt_fullscreen = ""
+theme.layout_txt_floating = ""
+theme.tasklist_disable_icon = false
+theme.icon_theme = "Papirus"
+theme.flag = {
+   france = theme.dir .. "/icons/fr.png",
+   mauritius = theme.dir .. "/icons/mu.png",
+   canada = theme.dir .. "/icons/ca.png",
+   cambodia = theme.dir .. "/icons/kh.png",
 }
 
 -- lain related
-theme.layout_txt_termfair           = "[termfair]"
-theme.layout_txt_centerfair         = "[centerfair]"
-theme.taglist_squares_sel           = gears.surface.load_from_shape(dpi(3), dpi(3), gears.shape.rectangle, theme.fg_focus)
-theme.taglist_squares_unsel         = gears.surface.load_from_shape(dpi(3), dpi(3), gears.shape.rectangle, theme.focus)
+theme.layout_txt_termfair = "[termfair]"
+theme.layout_txt_centerfair = "[centerfair]"
+theme.taglist_squares_sel = gears.surface.load_from_shape(dpi(3), dpi(3), gears.shape.rectangle, theme.fg_focus)
+theme.taglist_squares_unsel = gears.surface.load_from_shape(dpi(3), dpi(3), gears.shape.rectangle, theme.focus)
 
-theme.hotkeys_font = "PragmataPro Liga Bold " .. dpi(11)
-theme.hotkeys_description_font = "PragmataPro Liga " .. dpi(9)
+theme.hotkeys_font = "PragmataPro Nerd Font Bold " .. dpi(11)
+theme.hotkeys_description_font = "PragmataPro Nerd Font " .. dpi(9)
 -- Use pretty Unicode characters to represent special keys in hotkey hinter
 theme.hotkeys_popup_labels = {
-  Mod4 = " ⌥ ",
-  Mod1 = " ⎇ ",
-  Shift = " ⇧ ",
-  Control=" Ctrl ",
-  Left=' ← ',
-  Up=' ↑ ',
-  Right=' → ',
-  Down=' ↓ ',
-  Page_Down = " ↧ ",
-  Page_Up = " ↥ ",
-  Escape = " ⎋ ",
-  Return = " ↵ ",
-  Backspace = " ⌫ ",
-  Delete = " ⌦ ",
-  Insert = " ⎀ ",
-  space = " ⍽ ",
-  Tab = " ⇆ ",
-  ['#108'] = " ⌨ ",
-  F1 = " F1",
-  F2 = " F2",
-  F3 = " F3",
-  F4 = " F4",
-  F5 = " F5",
-  F6 = " F6",
-  F7 = " F7",
-  F8 = " F8",
-  F9 = " F9",
-  F10 = " F10",
-  F11 = " F11",
-  F12 = " F12",
-  ['#10']=" 1",
-  ['#11']=" 2",
-  ['#12']=" 3",
-  ['#13']=" 4",
-  ['#14']=" 5",
-  ['#15']=" 6",
-  ['#16']=" 7",
-  ['#17']=" 8",
-  ['#18']=" 9",
-  ['#19']=" 0",
-  ['#21']=" = ",
-  ['+']=" + ",
-  ['-']=" - ",
-  ['XF86AudioRaiseVolume'] = "🔊",
-  ['XF86AudioLowerVolume'] = "🔉",
-  ['XF86AudioMute'] = "🔇",
-  ['XF86Launch1'] = "🚀",
-  ['XF86AudioStop'] = "■",
-  ['XF86AudioPlay'] = "▶",
-  ['XF86AudioPrev'] = "⏪",
-  ['XF86AudioNext'] = "⏩",
-  ['XF86MonBrightnessUp'] = "🔆",
-  ['XF86MonBrightnessDown'] = "🔅",
-  ['XF86ScreenSaver'] = "🔒",
-  ['XF86Display'] = "🔅",
-  ['Print'] = " 🖶 ",
+   Mod4 = " ⌥ ",
+   Mod1 = " ⎇ ",
+   Shift = " ⇧ ",
+   Control = " Ctrl ",
+   Left = " ← ",
+   Up = " ↑ ",
+   Right = " → ",
+   Down = " ↓ ",
+   Page_Down = " ↧ ",
+   Page_Up = " ↥ ",
+   Escape = " ⎋ ",
+   Return = " ↵ ",
+   Backspace = " ⌫ ",
+   Delete = " ⌦ ",
+   Insert = " ⎀ ",
+   space = " ⍽ ",
+   Tab = " ⇆ ",
+   ["#108"] = " ⌨ ",
+   F1 = " F1",
+   F2 = " F2",
+   F3 = " F3",
+   F4 = " F4",
+   F5 = " F5",
+   F6 = " F6",
+   F7 = " F7",
+   F8 = " F8",
+   F9 = " F9",
+   F10 = " F10",
+   F11 = " F11",
+   F12 = " F12",
+   ["#10"] = " 1",
+   ["#11"] = " 2",
+   ["#12"] = " 3",
+   ["#13"] = " 4",
+   ["#14"] = " 5",
+   ["#15"] = " 6",
+   ["#16"] = " 7",
+   ["#17"] = " 8",
+   ["#18"] = " 9",
+   ["#19"] = " 0",
+   ["#21"] = " = ",
+   ["+"] = " + ",
+   ["-"] = " - ",
+   ["XF86AudioRaiseVolume"] = "🔊",
+   ["XF86AudioLowerVolume"] = "🔉",
+   ["XF86AudioMute"] = "🔇",
+   ["XF86Launch1"] = "🚀",
+   ["XF86AudioStop"] = "■",
+   ["XF86AudioPlay"] = "▶",
+   ["XF86AudioPrev"] = "⏪",
+   ["XF86AudioNext"] = "⏩",
+   ["XF86MonBrightnessUp"] = "🔆",
+   ["XF86MonBrightnessDown"] = "🔅",
+   ["XF86ScreenSaver"] = "🔒",
+   ["XF86Display"] = "🔅",
+   ["Print"] = " 🖶 ",
 }
 
 return theme
