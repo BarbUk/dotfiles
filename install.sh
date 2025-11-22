69e5f989ca454e411836704df3e48cf95ff2fbac
BarbUk <julien.virey@gmail.com>
2025-11-22 22:22:04 +0100

    Format scripts with shfmt

--- install.sh
+++ install.sh
@@ -5,19 +5,19 @@ set -e -o pipefail -o nounset
 readonly dotfiles_dir="$HOME/.dotfiles"
 
 if [ "$(uname)" = "Darwin" ]; then
-    LN='ln -sf'
-    mkdir -p "$HOME"/{.vim,.tmux/plugins}
+  LN='ln -sf'
+  mkdir -p "$HOME"/{.vim,.tmux/plugins}
 else
-    LN='ln -srf'
-    mkdir -p "$HOME"/{.vim,.tmux/plugins,.mpv,.mpd,.ncmpcpp,.config/rofi,.config/termite,.config/mpd}
-    mkdir -p "$HOME"/.vim/{swp,backup}
-    ${LN} "$dotfiles_dir/system/xinitrc" "$HOME/.xinitrc"
-    ${LN} "$dotfiles_dir/apps/mpv" "$HOME/.mpv/config"
-    ${LN} "$dotfiles_dir/apps/mpd.conf" "$HOME.config/mpd/mpd.conf"
-    ${LN} "$dotfiles_dir/apps/ncmpcpp" "$HOME/.ncmpcpp/config"
-    ${LN} "$dotfiles_dir/apps/compton.conf" "$HOME/.compton.conf"
-    ${LN} "$dotfiles_dir/apps/termite.dark" "$HOME/.config/termite/config"
-    ${LN} "$dotfiles_dir/apps/rofi.config" "$HOME/.config/rofi/config"
+  LN='ln -srf'
+  mkdir -p "$HOME"/{.vim,.tmux/plugins,.mpv,.mpd,.ncmpcpp,.config/rofi,.config/termite,.config/mpd}
+  mkdir -p "$HOME"/.vim/{swp,backup}
+  ${LN} "$dotfiles_dir/system/xinitrc" "$HOME/.xinitrc"
+  ${LN} "$dotfiles_dir/apps/mpv" "$HOME/.mpv/config"
+  ${LN} "$dotfiles_dir/apps/mpd.conf" "$HOME.config/mpd/mpd.conf"
+  ${LN} "$dotfiles_dir/apps/ncmpcpp" "$HOME/.ncmpcpp/config"
+  ${LN} "$dotfiles_dir/apps/compton.conf" "$HOME/.compton.conf"
+  ${LN} "$dotfiles_dir/apps/termite.dark" "$HOME/.config/termite/config"
+  ${LN} "$dotfiles_dir/apps/rofi.config" "$HOME/.config/rofi/config"
 fi
 
 mkdir -p "$HOME"/.config/htop
