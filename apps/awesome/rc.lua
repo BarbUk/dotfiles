5008a96df0998fafdc8f34fc923231dcb9c6bc81
BarbUk <julien.virey@gmail.com>
2026-02-01 18:32:27 +0400

    Add parent switcher

--- apps/awesome/rc.lua
+++ apps/awesome/rc.lua
@@ -4,6 +4,7 @@ pcall(require, "luarocks.loader")
 
 -- {{{ Required libraries
 local gears = require("gears")
+local awful = require("awful")
 local beautiful = require("beautiful")
 
 -- Garbage collection tuning
@@ -36,4 +37,22 @@ function sloppy_focus(c)
    end
    sloppyfocus_last.focus = true
 end
+
 client.connect_signal("mouse::enter", sloppy_focus)
+
+-- switch to parent after closing child window
+local function backham()
+   local s = awful.screen.focused()
+   local c = awful.client.focus.history.get(s, 0)
+   if c then
+      client.focus = c
+      c:raise()
+   end
+end
+
+-- attach to minimized state
+client.connect_signal("property::minimized", backham)
+-- attach to closed state
+client.connect_signal("unmanage", backham)
+-- ensure there is always a selected client during tag switching or logins
+tag.connect_signal("property::selected", backham)
