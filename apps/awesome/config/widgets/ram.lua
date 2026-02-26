1d940cab108ca6c0fd60b85acb160f0184e2b186
BarbUk <julien.virey@gmail.com>
2026-02-26 10:50:52 +0400

    Add a notification with top ram process

--- apps/awesome/config/widgets/ram.lua
+++ apps/awesome/config/widgets/ram.lua
@@ -1,11 +1,34 @@
 local lain = require("lain")
 local beautiful = require("beautiful")
+local awful = require("awful")
+local naughty = require("naughty")
 local markup = lain.util.markup
 
 local ram = lain.widget.mem({
    settings = function()
-      widget:set_markup(markup(beautiful.nord8, "󰍛") .. mem_now.perc .. "%")
+      widget:set_markup(markup(beautiful.nord8, "󰍛") .. mem_now.perc .. "% (" .. mem_now.used .. "MB)")
    end,
 })
+
+local ram_notification
+ram.widget:connect_signal("mouse::enter", function()
+   local cmd = [[bash -c "ps -Ao pmem,rss,comm --sort=-pmem --no-headers | ]]
+      .. [[head -n 5 | awk '{printf \"%s%% (%.1fMB) %s\\n\", $1, $2/1024, $3}'"]]
+   awful.spawn.easy_async(cmd, function(stdout)
+      ram_notification = naughty.notification({
+         title = "Top RAM process",
+         message = stdout,
+         timeout = 0,
+         hover_timeout = 0.5,
+      })
+   end)
+end)
+
+ram.widget:connect_signal("mouse::leave", function()
+   if ram_notification then
+      naughty.destroy(ram_notification)
+      ram_notification = nil
+   end
+end)
 
 return ram
