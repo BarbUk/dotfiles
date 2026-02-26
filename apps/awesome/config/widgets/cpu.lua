7c985b794bf76810aacc2bc4e5392e3b7612f6bd
BarbUk <julien.virey@gmail.com>
2026-02-26 10:44:22 +0400

    Add a notification with top cpu process

--- apps/awesome/config/widgets/cpu.lua
+++ apps/awesome/config/widgets/cpu.lua
@@ -1,5 +1,7 @@
 local lain = require("lain")
 local beautiful = require("beautiful")
+local awful = require("awful")
+local naughty = require("naughty")
 local markup = lain.util.markup
 
 local cpu = lain.widget.sysload({
@@ -7,5 +9,25 @@ local cpu = lain.widget.sysload({
       widget:set_markup(markup(beautiful.nord9, " ") .. load_1 .. " " .. load_5)
    end,
 })
+
+local cpu_notification
+cpu.widget:connect_signal("mouse::enter", function()
+   local cmd = [[bash -c "ps -Ao pcpu,comm --sort=-pcpu --no-headers | head -n 5 | awk '{print $1\"% \" $2}'"]]
+   awful.spawn.easy_async(cmd, function(stdout)
+      cpu_notification = naughty.notification({
+         title = "Top CPU process",
+         message = stdout,
+         timeout = 0,
+         hover_timeout = 0.5,
+      })
+   end)
+end)
+
+cpu.widget:connect_signal("mouse::leave", function()
+   if cpu_notification then
+      naughty.destroy(cpu_notification)
+      cpu_notification = nil
+   end
+end)
 
 return cpu
